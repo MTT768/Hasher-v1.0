ff07733497e2875bad35bbe689f83d686864f8cc
MTT768 <93826344+MTT768@users.noreply.github.com>
2022-05-02 11:16:56 +0300

    Delete Hasher v1.0.py

--- Hasher v1.0.py
+++
@@ -1,9 +0,0 @@
-import hashlib as hasher
-
-encoder = hasher.sha256()
-metin = input("Type What You Want to Encrypt:")
-encoder.update(metin.encode("utf-8"))
-hash = encoder.hexdigest()
-
-print("Şifreleme Başarıyla Tamamlandı:",hash)
-print("Coded by: MuhammedTr768")
